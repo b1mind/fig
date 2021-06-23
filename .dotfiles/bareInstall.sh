84fd0216bbba46b74c4965239ad143925923403d
b1mind <brent@1mindsolutions.com>
2021-06-22 18:48:12 -0600

    install script

---
+++ .dotfiles/bareInstall.sh
@@ -0,0 +1,16 @@
+# install bare script
+git clone --branch linux --bare https://github.com/b1mind/fig.git $HOME/.fig
+function config {
+   /usr/bin/git --git-dir=$HOME/.fig/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+fig checkout
+fig config --local status.showUntrackedFiles no
+echo ".fig" >> .gitignore
