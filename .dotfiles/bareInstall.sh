8e8eb9e7c73678601f7d1f54df8ece00faf5401b
b1mind <brent@1mindsolutions.com>
2021-06-22 19:30:47 -0600

    Install correction named function

--- .dotfiles/bareInstall.sh
+++ .dotfiles/bareInstall.sh
@@ -1,12 +1,12 @@
 # install bare script
 git clone --branch linux --bare https://github.com/b1mind/fig.git $HOME/.fig
-function config {
+function fig {
    /usr/bin/git --git-dir=$HOME/.fig/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-config checkout
+fig checkout
 if [ $? = 0 ]; then
-  echo "Checked out config.";
+  echo "Checked out fig.";
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
